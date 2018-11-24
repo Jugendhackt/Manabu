09225187536f64439db0e3373be37480c52e1300
Jonathan Smith-Chung <jonathan@Pinguin>
2018-11-24 20:07:30 +0100

    Join method

--- controllers/tree/displaytree.php
+++ controllers/tree/displaytree.php
@@ -1,1 +1,6 @@
 <?php
+$uid = $_SESSION['UID'];
+$pdo->prepare('SELECT subject.name as sName, weight, grade FROM grade LEFT JOIN subject on SID = subject.ID WHERE UID = :uid;');
+$getusersG = $pdo->bindParam('uid', $uid);
+$getusersG->execute();
+// got grades and subject
---
+++ smarty/templates_c/06865849177423c9ebd11afab153e6449f0ef9f0_0.file.index.tpl.php
@@ -0,0 +1,50 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-11-24 19:39:07
+  from '/home/jonathan/proj/Programme/jHackt/JugendHackt_Manabu/smarty/templates/index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bf99acb04e839_94756952',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '06865849177423c9ebd11afab153e6449f0ef9f0' => 
+    array (
+      0 => '/home/jonathan/proj/Programme/jHackt/JugendHackt_Manabu/smarty/templates/index.tpl',
+      1 => 1543060024,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bf99acb04e839_94756952 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_20496807145bf99acb04dc51_40913321', 'main');
+?>
+
+<?php $_smarty_tpl->inheritance->endChild($_smarty_tpl, "main.tpl");
+}
+/* {block 'main'} */
+class Block_20496807145bf99acb04dc51_40913321 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'main' => 
+  array (
+    0 => 'Block_20496807145bf99acb04dc51_40913321',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+<?php
+}
+}
+/* {/block 'main'} */
+}
