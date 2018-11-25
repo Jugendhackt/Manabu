ff292032aaee17ff4f165c232f4927fd860f81ee
Jonathan Smith-Chung <jonathan@Pinguin>
2018-11-25 09:55:07 +0100

    Added sql query

--- controllers/tree/displaybranch.php
+++ controllers/tree/displaybranch.php
@@ -5,9 +5,12 @@ if (!isset($_GET['sid'])) {
   $sid = $_GET['sid'];
 }
 $smarty->assign('pageTitle', 'Fach');
-$gets=$pdo->prepare('SELECT `subject.name`, `grade.grade` AS grade FROM grade LEFT JOIN subject ON SID = `subject.ID` WHERE `SID` = :sid AND uid = :uid;');
+$gets=$pdo->prepare('SELECT subject.name AS name, grade.grade FROM grade LEFT JOIN subject ON SID = subject.ID WHERE `SID` = :sid AND uid = :uid;');
 $gets->bindParam('sid', $sid);
 $gets->bindParam('uid', $_SESSION['uid']);
 $gets->execute();
+$subjectsGrades = $gets->fetchAll(PDO::FETCH_ASSOC);
+var_dump($subjectsGrades);
 
+//$smarty->asign('subjectname', $subjectsGrades[0]);
 $smarty->display('displaybranch.tpl');
