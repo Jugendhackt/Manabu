c6b206c43b4004897c4f9faec320200fc25e761f
climb <climbforthewin@gmail.com>
2018-11-25 12:05:50 +0100

    added redirects

--- web/login.php
+++ web/login.php
@@ -4,36 +4,38 @@ require __DIR__ . '/../incs/smartyimport.php';
 require  __DIR__ . '/../db/dbcon.php';
 session_start();
 
+if(!$_SESSION['logged_in']) {
 
-if (!empty($_GET['param'])) $param = $_GET['param'];
-else $param = '';
+    if (!empty($_GET['param'])) $param = $_GET['param'];
+    else $param = '';
 
-switch ($param) {
-  case 'login':
-    // process login:
-    $user = $_POST['user'];
-    $passwd = $_POST['passwd'];
+    switch ($param) {
+        case 'login':
+            // process login:
+            $user = $_POST['user'];
+            $passwd = $_POST['passwd'];
 
-    $prepLogin = $pdo->prepare("SELECT `passwd`, `nick`, `permissions`, `ID` FROM `user` WHERE `nick` = :nick");
-    $prepLogin->execute(array(':nick' => $user));
-    $res = $prepLogin->fetch(PDO::FETCH_ASSOC);
-    $dbPass = $res['passwd'];
-    $dbUser = $res['nick'];
-    $permissions = $res['permissions'];
-    $UID = $res['ID'];
+            $prepLogin = $pdo->prepare("SELECT `passwd`, `nick`, `permissions`, `ID` FROM `user` WHERE `nick` = :nick");
+            $prepLogin->execute(array(':nick' => $user));
+            $res = $prepLogin->fetch(PDO::FETCH_ASSOC);
+            $dbPass = $res['passwd'];
+            $dbUser = $res['nick'];
+            $permissions = $res['permissions'];
+            $UID = $res['ID'];
 
-    if(password_verify($passwd, $dbPass)) {
-        $_SESSION['logged_in'] = true;
-        $_SESSION['uid'] = $UID;
-        header('Location: tree.php');
-    } else {
-        $smarty->assign('pageTitle', 'Anmeldung fehlgeschlagen');
-        $smarty->display('login_failed.tpl');
+            if (password_verify($passwd, $dbPass)) {
+                $_SESSION['logged_in'] = true;
+                $_SESSION['uid'] = $UID;
+                header('Location: tree.php');
+            } else {
+                $smarty->assign('pageTitle', 'Anmeldung fehlgeschlagen');
+                $smarty->display('login_failed.tpl');
+            }
+
+            break;
+        default: // show login login form
+            $smarty->assign('pageTitle', 'Anmelden');
+            $smarty->display('login_form.tpl');
+            break;
     }
-
-    break;
-  default: // show login login form
-    $smarty->assign('pageTitle', 'Anmelden');
-    $smarty->display('login_form.tpl');
-    break;
-}
+} else header('Location: tree.php');
